08e8aa76b087937c99ab7355810f5582f5babd30
Azat Yamalov <yamalov.azat@live.com>
2022-08-31 13:21:33 +0300

    fix building tree with opened nodes

--- pl-data-tree.js
+++ pl-data-tree.js
@@ -70,12 +70,13 @@ class PlDataTree extends PlElement {
                 return true;
             } else {
                 let parent = openedSet.get(i[pkey]);
-                if (parent) {
+                while(parent) {
                     i._level = parent._level + 1;
                     i._pitem = parent;
                     parent._childrenCount++;
-                    return true
+                    parent = openedSet.get(i._pitem[pkey]);
                 }
+                return true
             }
         });
         vData.load = this.in.load;
